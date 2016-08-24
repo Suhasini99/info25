4d946277bbe3c844127f623816e8110e8b9c758c
sdarekar <sdarekar@admin-PC>
2016-08-24 10:55:29 +0530

    second commit

--- src/value/Max.java
+++ src/value/Max.java
@@ -4,6 +4,8 @@ public class Max {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//second commit
+		//just info
 System.out.println("hellooo welcome to new world");
 	}
 
